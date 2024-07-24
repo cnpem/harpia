9d899bef4194669703dc67c65c0d3e4cffcc4a8c
ricardo.grangeiro <ricardo.grangeiro@lnls.br>
2024-07-24 09:39:35 -0300

    refactor: cython functions templates

--- harpia/quantification/quantification.pyx
+++ harpia/quantification/quantification.pyx
@@ -3,13 +3,26 @@ cimport numpy
 import numpy as np
 from libcpp cimport bool
 
+# Extern declaration for the fraction calculation function from C/C++ library
 cdef extern from "../../include/quantification/fraction.h":
     void fraction(int* image, int* output, int xsize, int ysize, int zsize)
 
 def compute_fraction(numpy.int32_t[:,:,:] image,
-                     numpy.int32_t [:,:,:]  output,
+                     numpy.int32_t[:,:,:] output,
                      int xsize, int ysize, int zsize):
-    
+    """
+    Compute the fraction of a 3D image.
+
+    Parameters:
+        image (numpy.int32_t[:,:,:]): Input 3D image array.
+        output (numpy.int32_t[:,:,:]): Output 3D array to store the fraction result.
+        xsize (int): Size of the x-dimension.
+        ysize (int): Size of the y-dimension.
+        zsize (int): Size of the z-dimension.
+
+    Returns:
+        None
+    """
     # Get pointers to the data
     cdef int* image_ptr = <int*>&image[0,0,0]
     cdef int* output_ptr = <int*>&output[0,0,0]
@@ -17,13 +30,26 @@ def compute_fraction(numpy.int32_t[:,:,:] image,
     return fraction(image_ptr, output_ptr, xsize, ysize, zsize)
 
 
+# Extern declaration for the perimeter calculation function from C/C++ library
 cdef extern from "../../include/quantification/perimeter.h":
     void perimeter(int* image, unsigned int* output, int xsize, int ysize, int zsize)
 
 def compute_perimeter(numpy.int32_t[:,:,:] image,
-                      numpy.uint32_t [:,:,:]  output,
+                      numpy.uint32_t[:,:,:] output,
                       int xsize, int ysize, int zsize):
+    """
+    Compute the perimeter of a 3D image.
 
+    Parameters:
+        image (numpy.int32_t[:,:,:]): Input 3D image array.
+        output (numpy.uint32_t[:,:,:]): Output 3D array to store the perimeter result.
+        xsize (int): Size of the x-dimension.
+        ysize (int): Size of the y-dimension.
+        zsize (int): Size of the z-dimension.
+
+    Returns:
+        None
+    """
     # Get pointers to the data
     cdef int* image_ptr = <int*>&image[0,0,0]
     cdef unsigned int* output_ptr = <unsigned int*>&output[0,0,0]
@@ -31,28 +57,54 @@ def compute_perimeter(numpy.int32_t[:,:,:] image,
     return perimeter(image_ptr, output_ptr, xsize, ysize, zsize)
 
 
-
+# Extern declaration for the area calculation function from C/C++ library
 cdef extern from "../../include/quantification/area.h":
     void area(int* image, unsigned int* output, int xsize, int ysize, int zsize, bool type)
 
 def compute_area(numpy.int32_t[:,:,:] image,
-                 numpy.uint32_t [:,:,:]  output,
+                 numpy.uint32_t[:,:,:] output,
                  int xsize, int ysize, int zsize, bool type):
+    """
+    Compute the area of a 3D image.
 
+    Parameters:
+        image (numpy.int32_t[:,:,:]): Input 3D image array.
+        output (numpy.uint32_t[:,:,:]): Output 3D array to store the area result.
+        xsize (int): Size of the x-dimension.
+        ysize (int): Size of the y-dimension.
+        zsize (int): Size of the z-dimension.
+        type (bool): Type parameter for the area calculation.
+
+    Returns:
+        None
+    """
     # Get pointers to the data
     cdef int* image_ptr = <int*>&image[0,0,0]
     cdef unsigned int* output_ptr = <unsigned int*>&output[0,0,0]
 
-    return area(image_ptr, output_ptr, xsize, ysize, zsize,type)
+    return area(image_ptr, output_ptr, xsize, ysize, zsize, type)
 
 
+# Extern declaration for the volume calculation function from C/C++ library
 cdef extern from "../../include/quantification/volume.h":
     void volume(int* image, unsigned int* output, int xsize, int ysize, int zsize)
 
 def compute_volume(numpy.int32_t[:,:,:] image,
-                 numpy.uint32_t [:,:,:]  output,
-                 int xsize, int ysize, int zsize):
+                   numpy.uint32_t[:,:,:] output,
+                   int xsize, int ysize, int zsize):
+    """
+    Compute the volume of a 3D image.
 
+    Parameters:
+        image (numpy.int32_t[:,:,:]): Input 3D image array.
+        output (numpy.uint32_t[:,:,:]): Output 3D array to store the volume result.
+        xsize (int): Size of the x-dimension.
+        ysize (int): Size of the y-dimension.
+        zsize (int): Size of the z-dimension.
+
+    Returns:
+        None
+    """
     # Get pointers to the data
     cdef int* image_ptr = <int*>&image[0,0,0]
     cdef unsigned int* output_ptr = <unsigned int*>&output[0,0,0]
--- harpia/threshold/threshold.pyx
+++ harpia/threshold/threshold.pyx
@@ -2,13 +2,13 @@ cimport cython
 cimport numpy as np
 from libcpp cimport bool
 
-# Define the fused type
+# Define the fused type for numeric types: float, int, unsigned int
 ctypedef fused numeric:
     float
     int
     unsigned int
 
-# Local Gaussian Threshold
+# Extern declaration for the Local Gaussian Threshold function from C/C++ library
 cdef extern from "../../include/threshold/adaptative_gaussian.h":
     void local_gaussian_threshold[numeric](numeric* image, float* output, int rows, int cols, int depth, float sigma, float weight, bool type)
 
@@ -16,12 +16,28 @@ def local_gaussian(np.ndarray[numeric, ndim=3] image,
                    np.ndarray[np.float32_t, ndim=3] output,
                    int rows, int cols, int depth,
                    float sigma, float weight, bool type):
+    """
+    Apply a local Gaussian threshold to a 3D image.
+
+    Parameters:
+        image (np.ndarray[numeric, ndim=3]): Input 3D image array.
+        output (np.ndarray[np.float32_t, ndim=3]): Output 3D array to store the thresholded result.
+        rows (int): Number of rows in the image.
+        cols (int): Number of columns in the image.
+        depth (int): Number of depth slices in the image.
+        sigma (float): Standard deviation for Gaussian kernel.
+        weight (float): Weight parameter for thresholding.
+        type (bool): Type of thresholding (specific to implementation).
+
+    Returns:
+        None
+    """
     return local_gaussian_threshold(&image[0,0,0],
                                     &output[0,0,0],
                                     rows, cols, depth,
                                     sigma, weight, type)
 
-# Local Mean Threshold
+# Extern declaration for the Local Mean Threshold function from C/C++ library
 cdef extern from "../../include/threshold/adaptative_mean.h":
     void local_mean_threshold[numeric](numeric* image, float* output, float weight,
                                        int rows, int cols, int depth,
@@ -32,13 +48,30 @@ def local_mean(np.ndarray[numeric, ndim=3] image,
                float weight,
                int rows, int cols, int depth,
                int rows_kernel, int cols_kernel, int depth_kernel):
+    """
+    Apply a local mean threshold to a 3D image.
+
+    Parameters:
+        image (np.ndarray[numeric, ndim=3]): Input 3D image array.
+        output (np.ndarray[np.float32_t, ndim=3]): Output 3D array to store the thresholded result.
+        weight (float): Weight parameter for thresholding.
+        rows (int): Number of rows in the image.
+        cols (int): Number of columns in the image.
+        depth (int): Number of depth slices in the image.
+        rows_kernel (int): Number of rows in the kernel.
+        cols_kernel (int): Number of columns in the kernel.
+        depth_kernel (int): Number of depth slices in the kernel.
+
+    Returns:
+        None
+    """
     return local_mean_threshold(&image[0,0,0],
                                 &output[0,0,0],
                                 weight,
                                 rows, cols, depth,
                                 rows_kernel, cols_kernel, depth_kernel)
 
-# Niblack Threshold
+# Extern declaration for the Niblack Threshold function from C/C++ library
 cdef extern from "../../include/threshold/niblack.h":
     void niblack_threshold[numeric](numeric* image, float* output, float weight,
                                     int rows, int cols, int depth,
@@ -49,13 +82,30 @@ def niblack(np.ndarray[numeric, ndim=3] image,
             float weight,
             int rows, int cols, int depth,
             int rows_kernel, int cols_kernel, int depth_kernel):
+    """
+    Apply a Niblack threshold to a 3D image.
+
+    Parameters:
+        image (np.ndarray[numeric, ndim=3]): Input 3D image array.
+        output (np.ndarray[np.float32_t, ndim=3]): Output 3D array to store the thresholded result.
+        weight (float): Weight parameter for thresholding.
+        rows (int): Number of rows in the image.
+        cols (int): Number of columns in the image.
+        depth (int): Number of depth slices in the image.
+        rows_kernel (int): Number of rows in the kernel.
+        cols_kernel (int): Number of columns in the kernel.
+        depth_kernel (int): Number of depth slices in the kernel.
+
+    Returns:
+        None
+    """
     return niblack_threshold(&image[0,0,0],
                              &output[0,0,0],
                              weight,
                              rows, cols, depth,
                              rows_kernel, cols_kernel, depth_kernel)
 
-# Sauvola Threshold
+# Extern declaration for the Sauvola Threshold function from C/C++ library
 cdef extern from "../../include/threshold/sauvola.h":
     void sauvola_threshold[numeric](numeric* image, float* output, float weight, float range,
                                     int rows, int cols, int depth,
@@ -66,6 +116,24 @@ def sauvola(np.ndarray[numeric, ndim=3] image,
             float weight, float range,
             int rows, int cols, int depth,
             int rows_kernel, int cols_kernel, int depth_kernel):
+    """
+    Apply a Sauvola threshold to a 3D image.
+
+    Parameters:
+        image (np.ndarray[numeric, ndim=3]): Input 3D image array.
+        output (np.ndarray[np.float32_t, ndim=3]): Output 3D array to store the thresholded result.
+        weight (float): Weight parameter for thresholding.
+        range (float): Range parameter for thresholding.
+        rows (int): Number of rows in the image.
+        cols (int): Number of columns in the image.
+        depth (int): Number of depth slices in the image.
+        rows_kernel (int): Number of rows in the kernel.
+        cols_kernel (int): Number of columns in the kernel.
+        depth_kernel (int): Number of depth slices in the kernel.
+
+    Returns:
+        None
+    """
     return sauvola_threshold(&image[0,0,0],
                              &output[0,0,0],
                              weight, range,
