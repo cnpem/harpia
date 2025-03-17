fee791b42761127cd60404fe5444aae26b9b9f05
Camila Machado de Araújo <camila.araujo@lnls.br>
2025-03-17 16:57:02 -0300

    docs: add chunkedExecutor documentation

--- include/common/chunkedExecutor.h
+++ include/common/chunkedExecutor.h
@@ -4,17 +4,90 @@
 #include <stdio.h>
 #include <iostream>
 
+/**
+ * @brief Wrapper function to execute a CUDA function in chunks.
+ * 
+ * Splits a 3D image into smaller chunks based on available GPU memory and processes each chunk separately.
+ * The image is divided along the z-axis to simplify memory management and computation.
+ * Each chunk is passed to the specified CUDA function for processing, ensuring efficient execution 
+ * while preventing memory overflows.
+ *
+ * @tparam Func Type of the CUDA function to execute.
+ * @tparam dtype Data type of the image (e.g., uint8_t, float).
+ * @tparam Args Variadic template arguments for additional parameters to the CUDA function.
+ * @param func The CUDA function to execute.
+ * @param ncopies Number of copies of the image data needed to execute the algorithm.
+ * @param safetyMargin Fraction of available GPU memory to use (0-1).
+ * @param image Pointer to the input image data.
+ * @param output Pointer to the output image data.
+ * @param xsize Width of the image.
+ * @param ysize Height of the image.
+ * @param zsize Depth of the image.
+ * @param verbose Verbosity flag (1 for debug output, 0 for silent execution).
+ * @param args Additional arguments passed to the CUDA function.
+ */
 template <typename Func, typename dtype, typename... Args>
 void chunkedExecutor(Func func, int ncopies, const float safetyMargin, dtype* image, dtype* output,
                      const int xsize, const int ysize, const int zsize, const int verbose,
                      Args... args);
 
+/**
+ * @brief Specialized chunked execution for morphological operations using a kernel.
+ *
+ * This function is optimized for executing morphological operations that use a kernel.
+ * It splits the 3D image into smaller chunks based on available GPU memory and processes
+ * each chunk separately using the specified CUDA function.
+ *
+ * When a kernel operation is applied, padding is required to ensure correct border handling.
+ * In cases where two consecutive kernel operations are performed, the first execution applies
+ * double padding to guarantee that the second operation also has the necessary padding.
+ *
+ * @tparam Func Type of the CUDA function to execute.
+ * @tparam dtype Data type of the image.
+ * @tparam Args Variadic template arguments for additional parameters.
+ * @param func The CUDA function to execute.
+ * @param ncopies Number of copies of the image data needed to execute the algorithm.
+ * @param safetyMargin Fraction of available GPU memory to use.
+ * @param kernelOperations Number of kernel operations to be performed.
+ * @param image Pointer to the input image data.
+ * @param output Pointer to the output image data.
+ * @param xsize Width of the image.
+ * @param ysize Height of the image.
+ * @param zsize Depth of the image.
+ * @param verbose Verbosity flag.
+ * @param kernel Pointer to the kernel data.
+ * @param kernel_xsize Width of the kernel.
+ * @param kernel_ysize Height of the kernel.
+ * @param kernel_zsize Depth of the kernel.
+ * @param args Additional arguments passed to the CUDA function.
+ */
 template <typename Func, typename dtype, typename... Args>
 void chunkedExecutorKernel(Func func, int ncopies, const float safetyMargin,
                            const int kernelOperations, dtype* image, dtype* output, const int xsize,
                            const int ysize, const int zsize, const int verbose, int* kernel,
                            int kernel_xsize, int kernel_ysize, int kernel_zsize, Args... args);
 
+/**
+ * @brief Specialized chunked execution for executing geodesic morphological operations.
+ *
+ * This function is designed for geodesic operations that use an 8-connectivity fixed kernel of ones
+ * and require both an image and a mask as input.
+ *
+ * @tparam Func Type of the CUDA function to execute.
+ * @tparam dtype Data type of the image.
+ * @tparam Args Variadic template arguments for additional parameters.
+ * @param func The CUDA function to execute.
+ * @param ncopies Number of copies of the image data.
+ * @param safetyMargin Fraction of available GPU memory to use.
+ * @param image Pointer to the input image data.
+ * @param mask Pointer to the mask data.
+ * @param output Pointer to the output image data.
+ * @param xsize Width of the image.
+ * @param ysize Height of the image.
+ * @param zsize Depth of the image.
+ * @param verbose Verbosity flag.
+ * @param args Additional arguments passed to the CUDA function.
+ */
 template <typename Func, typename dtype, typename... Args>
 void chunkedExecutorMask(Func func, int ncopies, const float safetyMargin, const int flag_chain,
                          dtype* image, dtype* mask, dtype* output, const int xsize, const int ysize,
