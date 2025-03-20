b905326c5efbf6bdc2f8f0491c9e4d0a862538fc
Camila Machado de Araújo <camila.araujo@lnls.br>
2025-03-20 12:37:01 -0300

    tests: repeat tests with 4 gpus
    The execution time with 2 gpus was also 2 times faster then using only 1 gpu, as expected. However, using 4 gpus, the results are much slower. The 4 gpus tets was repeated to guarantee this is always the outcome. This problem must be investigated.

--- tests_python/benchmark/benchmark.py
+++ tests_python/benchmark/benchmark.py
@@ -5,8 +5,8 @@ import csv
 import h5py
 
 # Grayscale morphology operations
-from skimage.morphology import (       
-    erosion, dilation, closing, opening, 
+from skimage.morphology import (
+    erosion, dilation, closing, opening,
     white_tophat, black_tophat, reconstruction
 )
 
@@ -16,7 +16,7 @@ from skimage.morphology import binary_erosion, binary_dilation, binary_closing, 
 # Workaround to allow importing harpia python module locally
 import sys
 import os
-sys.path.append(os.path.abspath(os.path.join(os.getcwd(), os.pardir, os.pardir))) 
+sys.path.append(os.path.abspath(os.path.join(os.getcwd(), os.pardir, os.pardir)))
 
 # Custom morphology operations from harpia for binary images
 from harpia.morphology.operations_binary import (
@@ -66,18 +66,18 @@ def smooth_sk(image, selem):
 
 # Instruction: Uncomment the image for which tests will be executed.
 
-# IMAGE 1
-print("reading small image...")
-xsize = 190
-ysize = 207
-zsize_original = 100
-zsize = 100
-path_grayscale = "../../example_images/grayscale/crua_A_190x207x100_16b.raw"
-path_binary = "../../example_images/binary/crua_A_190x207x100_16b.raw"
-image_grayscale = image.load(path_grayscale, xsize, ysize, zsize,'uint16')
-image_binary = image.load(path_binary, xsize, ysize, zsize,'uint16')
-img_num = 1
-print("fineshed reading small image!")
+# # IMAGE 1
+# print("reading small image...")
+# xsize = 190
+# ysize = 207
+# zsize_original = 100
+# zsize = 100
+# path_grayscale = "../../example_images/grayscale/crua_A_190x207x100_16b.raw"
+# path_binary = "../../example_images/binary/crua_A_190x207x100_16b.raw"
+# image_grayscale = image.load(path_grayscale, xsize, ysize, zsize,'uint16')
+# image_binary = image.load(path_binary, xsize, ysize, zsize,'uint16')
+# img_num = 1
+# print("fineshed reading small image!")
 
 # # IMAGE 2 (possibily with problem)
 # print("reading big image...")
@@ -102,18 +102,18 @@ print("fineshed reading small image!")
 # img_num = 3
 # print("fineshed reading medium image!")
 
-# # IMAGE 4 
-# print("reading big image...")
-# xsize = 2052
-# ysize = 2052
-# zsize = 2048
+# IMAGE 4
+print("reading big image...")
+xsize = 2052
+ysize = 2052
+zsize = 2048
 
-# path_grayscale = "../../../../../../../../labs/tepui/home/camila.araujo/work/harpia/example_images/grayscale/Recon_2052x2052x2048_32bits.raw"
-# path_binary = "../../../../../../../../labs/tepui/home/camila.araujo/work/harpia/example_images/binary/Recon_2052x2052x2048_16bits.raw"
-# image_grayscale = image.load(path_grayscale, xsize, ysize, zsize,'float32')
-# image_binary = image.load(path_binary, xsize, ysize, zsize,'uint16')
-# img_num = 4
-# print("fineshed reading big image!")
+path_grayscale = "../../../../../../../../labs/tepui/home/camila.araujo/work/harpia/example_images/grayscale/Recon_2052x2052x2048_32bits.raw"
+path_binary = "../../../../../../../../labs/tepui/home/camila.araujo/work/harpia/example_images/binary/Recon_2052x2052x2048_16bits.raw"
+image_grayscale = image.load(path_grayscale, xsize, ysize, zsize,'float32')
+image_binary = image.load(path_binary, xsize, ysize, zsize,'uint16')
+img_num = 4
+print("fineshed reading big image!")
 
 #Kernel
 kernel = custum_kernel3D()
@@ -123,50 +123,50 @@ kernel = custum_kernel3D()
 #############
 
 machine = 'harriet'
-ngpus = 1
+ngpus = 4
 gpuMemory = 0.2
 repetitions = 11
 
-# # GRAYSCALE 
-# operations = [
-#     ("Erosion 3D grayscale", erosion, erosion_grayscale, "erosion_grayscale"),
-#     ("Dilation 3D grayscale", dilation, dilation_grayscale, "dilation_grayscale"),
-#     ("Closing 3D grayscale", closing, closing_grayscale, "closing_grayscale"),
-#     ("Opening 3D grayscale", opening, opening_grayscale, "opening_grayscale"),
-# ]
+# GRAYSCALE
+operations = [
+    ("Erosion 3D grayscale", erosion, erosion_grayscale, "erosion_grayscale"),
+    ("Dilation 3D grayscale", dilation, dilation_grayscale, "dilation_grayscale"),
+    ("Closing 3D grayscale", closing, closing_grayscale, "closing_grayscale"),
+    ("Opening 3D grayscale", opening, opening_grayscale, "opening_grayscale"),
+]
 
-# images = [    
-#     ("int32", f"image{img_num}_int32_grayscale"),
-#     ("uint32", f"image{img_num}_uint32_grayscale"),
-#     ("float32", f"image{img_num}_float32_grayscale"),
-#    ]
+images = [
+    ("int32", f"image{img_num}_int32_grayscale"),
+    ("uint32", f"image{img_num}_uint32_grayscale"),
+    ("float32", f"image{img_num}_float32_grayscale"),
+   ]
 
-# #Iterate over operations and images
-# for img in images:
-#     image_input = image_grayscale.astype(dtype=img[0])
-#     for operation in operations:
-#         csv_file = f"results{img_num}/{machine}_{ngpus}gpu_{operation[3]}_{img[1]}.csv"
-#         # Attempt to run the test
-#         results_df = tests.run(
-#             csv_file, image_input, operation, machine, ngpus, True, repetitions, gpuMemory, kernel
-#         )
+#Iterate over operations and images
+for img in images:
+    image_input = image_grayscale.astype(dtype=img[0])
+    for operation in operations:
+        csv_file = f"results{img_num}/{machine}_{ngpus}gpu_{operation[3]}_{img[1]}.csv"
+        # Attempt to run the test
+        results_df = tests.run(
+            csv_file, image_input, operation, machine, ngpus, True, repetitions, gpuMemory, kernel
+        )
 
-# BINARY 
+# BINARY
 operations = [
     ("Erosion 3D binary", erosion, erosion_binary, "erosion_binary"),
-    ("Dilation 3D binary", dilation, dilation_binary, "dilation_binary"),    
+    ("Dilation 3D binary", dilation, dilation_binary, "dilation_binary"),
     ("Closing 3D binary", closing, closing_binary, "closing_binary"),
     ("Opening 3D binary", opening, opening_binary, "opening_binary"),
     ("Smoothing 3D binary", smooth_sk, smooth_binary, "smooth_binary"),
 ]
 
 images_binary = [
-    # ("int8", f"image{img_num}_int8_binary"),
-    # ("uint8", f"image{img_num}_uint8_binary"),
-    # ("int16", f"image{img_num}_int16_binary"),
-    ("uint16", f"image{img_num}_uint16_binary"),    
-    # ("int32", f"image{img_num}_int32_binary"),
-    # ("uint32", f"image{img_num}_uint32_binary"),
+    # ("int8", f"image{img_num}_int8_binary"), cause segmentation fault for Big Images (image 4)
+    # ("uint8", f"image{img_num}_uint8_binary"), cause segmentation fault for Big Images (image 4)
+    ("int16", f"image{img_num}_int16_binary"),
+    ("uint16", f"image{img_num}_uint16_binary"),
+    ("int32", f"image{img_num}_int32_binary"),
+    ("uint32", f"image{img_num}_uint32_binary"),
    ]
 
 #Iterate over operations and images
