30924654ead9d378b3e258ba80122dbb28c74b06
ricardo.grangeiro <ricardo.grangeiro@lnls.br>
2025-06-11 16:53:28 -0300

    feat: lbp implementation

--- harpia/localBinaryPattern/lbp.pyx
+++ harpia/localBinaryPattern/lbp.pyx
@@ -12,4 +12,21 @@ ctypedef fused numeric:
     unsigned int
 #---------------------------------------------------------------------------------------------------
 cdef extern from "../../include/localBinaryPattern/lbp.h":
-    void localBinaryPattern(numeric* hostImage, float* hostOutput, int xsize, int ysize, int zsize)
+    void localBinaryPattern[numeric](numeric* hostImage, float* hostOutput, int xsize, int ysize, int zsize)
+
+def LBP(numpy.ndarray[numeric, ndim=3] hostImage,
+                   numpy.ndarray[numpy.float32_t, ndim=3] hostOutput = None,
+                   float sigma = 1, int type3d=1,
+                   int verbose = 0, float gpuMemory = 0.4, int ngpus = -1
+                   ):
+
+    isize = Size(hostImage)
+
+    if hostOutput is None:
+        hostOutput = numpy.empty((isize.z, isize.y, isize.x), dtype=numpy.float32)
+
+    localBinaryPattern(&hostImage[0, 0, 0],
+                          &hostOutput[0, 0, 0],
+                          isize.y, isize.x, isize.z)
+    
+    return hostOutput
