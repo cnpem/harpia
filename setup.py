9a83c7b6150f9c4f30f729e0a04e5aed3ae1b77c
Allan Pinto <allan.pinto@lnls.br>
2025-09-30 06:34:07 -0300

    setup.py: remove script_args and enforce manylinux2014 wheel tag
    - Removed hardcoded script_args from setup() to avoid overriding build process
    - Added bdist_wheel options to force manylinux2014_x86_64 platform tag

--- setup.py
+++ setup.py
@@ -230,7 +230,6 @@ setup(
     #   cmdclass=versioneer.get_cmdclass(),
     version=version['__version__'],
     description="CUDA extension for Python",
-    script_args=["build_ext", "--inplace", "bdist_wheel"],
     ext_modules=cythonize(
         ext_modules,
         compiler_directives=dict(
@@ -238,5 +237,8 @@ setup(
         ),
     ),
     cmdclass={"build_ext": custom_build_ext},
+    options={
+        "bdist_wheel": {"plat_name": "manylinux2014_x86_64"}
+    },
     zip_safe=False,
 )
